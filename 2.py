d504dc7e8fa93c71a40702092097e473bb670a4a
Parth Pandey <155581893+parthbuilds@users.noreply.github.com>
2025-01-13 01:09:45 +0530

    basics

---
+++ 2.py
@@ -0,0 +1,67 @@
+# x = 20
+# y = "parth"
+# print (x)
+# print (y) 
+
+
+# z = str(5)
+# print (z)
+
+# print (type(z))
+
+
+# #variable dif case sensitive 
+# a = 23
+# A = "parth"
+
+
+# x , y , z = 1 , 2 , 3
+
+# print (x)
+
+
+
+# fruits = ["apple ","orange","cherry"]
+# print (fruits)
+
+
+# x,y,z = fruits
+# print (x)
+# print (y)
+# print (z)
+
+
+# print (x, y)
+
+
+
+# m = memoryview(bytes(5))
+
+# p = list(("apple", "banana", "cherry"))
+
+# print(p)
+
+# x = "1"
+
+# y = str(x)
+# print(y)
+
+# y = ('it\'s a tuplle')
+# print (y)
+
+
+a = "hello world"
+print(a[2])
+
+print (len(a))
+
+print(a[2:5])
+print(a[:4])
+
+print(a[0:13:2])
+
+print(a.upper())
+
+
+print("hello this is a \n test file")
+
