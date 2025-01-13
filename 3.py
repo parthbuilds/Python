e6d9819b727f3f9215c1d0d6d646545c0166fef7
Parth Pandey <155581893+parthbuilds@users.noreply.github.com>
2025-01-13 16:25:04 +0530

    list

---
+++ 3.py
@@ -0,0 +1,35 @@
+# #list 
+
+# list = ["apple", "banana", "mango"]
+
+# print (list)
+# print(len(list))
+
+# # print(list[1])
+# print(list[-1])
+
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+  
+# list[1] = "mugu"
+# print(list)
+
+# list.append("orange")
+# print(list)
+
+# list comprehension
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+newlist = []
+for x in fruits :
+    if "a" in x:
+        newlist.append(x)
+        
+print(newlist)
+
+
+a = fruits.index("banana")
+print(a)
